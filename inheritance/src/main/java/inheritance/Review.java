d05bab2c2675905b8b20f1514af0138077695a71
river-ceanne <snvencer@gmail.com>
2019-06-04 10:42:15 -0700

    Review class initially creatded with base funcs

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -1,4 +1,45 @@
 package inheritance;
 
 public class Review {
+    // Create a class to represent a Review. Each Review should have a body, an author, and a number of stars.
+    private String author;
+    private String body;
+    private int stars;
+
+    public Review(){}
+
+    public Review(String author, String body, int stars){
+        this.author = author;
+        this.body = body;
+        this.stars = stars;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
